d4693c3c0a8c45fa48549e361470ab98b62b992a
Brooklin Myers <Brooklinjazz@hotmail.com>
2018-06-11 18:24:29 -0700

    persist todos state

--- src/index.js
+++ src/index.js
@@ -5,8 +5,24 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import todoApp from './reducers';
 import App from './components/App';
+import { loadState, saveState } from './localstorage'
 
-const store = createStore(todoApp);
+// define the persistedState as what is loaded from local storage
+const persistedState = loadState()
+
+const store = createStore(
+  todoApp,
+  persistedState
+);
+
+// store.subscribe() will be called every time the state changes.
+// essentially it adds a listener to state changes.
+store.subscribe(() => {
+  console.log('statechange', persistedState);
+  saveState({
+    todos: store.getState().todos
+  });
+})
 
 render(
   <Provider store={store}>
