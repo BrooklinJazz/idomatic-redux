f0219082ab6b12f686caa0b2050096d831bca9d9
Brooklin Myers <Brooklinjazz@hotmail.com>
2018-06-12 15:52:42 -0700

    fix activeStyling for filter Link

--- src/components/FilterLink.js
+++ src/components/FilterLink.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { NavLink } from 'react-router-dom'
 
 const FilterLink = ({ filter, children }) => (
-  <NavLink 
-  to={filter === 'all' ? '' : filter}
-  activeStyle={{
-    textDecoration: 'none',
-    color: 'black',
-  }}
+  <NavLink
+    exact to={filter === 'all' ? '' : '/' + filter}
+    activeStyle={{
+      textDecoration: 'none',
+      color: 'black',
+    }}
   >
-  {children}
+    {children}
   </NavLink>
 )
 
