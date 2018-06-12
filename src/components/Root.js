929901c545488a848e23ba2a08ed4c0206d6a5ae
Brooklin Myers <Brooklinjazz@hotmail.com>
2018-06-12 12:09:31 -0700

    configure Routing in Root component

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import Provider from 'react-redux'
+import { Provider } from 'react-redux'
+import { Router, Route } from 'react-router'
+import createBrowserHistory from '../history'
 import App from './App'
 
 // {store} acts as ES6 syntax to utilize props.store as store.
-const Root = ({ store }) => {
+const Root = ({ store }) => (
     <Provider store={store}>
-        <App />
+        <Router history={createBrowserHistory} >
+            <Route path='/' component={App} />
+        </Router>
     </Provider>
-}
+)
 
 export default Root
