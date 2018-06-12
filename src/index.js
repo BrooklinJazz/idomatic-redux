ae5ed180dec5f808dd1a8a19ce9cd0f76bbb6253
Brooklin Myers <Brooklinjazz@hotmail.com>
2018-06-12 10:32:47 -0700

    configure entry point with Root component

--- src/index.js
+++ src/index.js
@@ -3,30 +3,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import todoApp from './reducers';
 import App from './components/App';
-import { loadState, saveState } from './localstorage'
-import { throttle } from 'lodash'
+import Root from './components/Root'
+import configureStore from './configureStore'
 
-// define the persistedState as what is loaded from local storage
-const persistedState = loadState()
-
-const store = createStore(
-  todoApp,
-  persistedState
-);
-
-// store.subscribe() will be called every time the state changes.
-// essentially it adds a listener to state changes.
-store.subscribe(throttle(() => {
-  saveState({
-    todos: store.getState().todos
-  });
-}, 1000))
+const store = configureStore()
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  // simplified component for App and Providers
+  <Root store={store}/>,
   document.getElementById('root')
 );
