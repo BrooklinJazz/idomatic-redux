ebe0845c51436466adb4407d9cd4aac82d7292ba
Brooklin Myers <Brooklinjazz@hotmail.com>
2018-06-12 12:11:31 -0700

    remove unused createstore import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import App from './components/App';
 import Root from './components/Root'
 import configureStore from './configureStore'
