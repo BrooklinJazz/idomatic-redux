29638144f18788f1452fafad2182bdda020e1b4a
Brooklin Myers <Brooklinjazz@hotmail.com>
2018-06-12 23:22:39 -0700

    refactor to make components unaware of state structure

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -2,33 +2,13 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router'
 import { toggleTodo } from '../actions';
 import TodoList from './TodoList';
-
-const getVisibleTodos = (todos, filter) => {
-  switch (filter) {
-    case 'all':
-      return todos;
-    case 'completed':
-      return todos.filter(t => t.completed);
-    case 'active':
-      return todos.filter(t => !t.completed);
-    default:
-      throw new Error(`Unknown filter: ${filter}.`);
-  }
-};
+import { getVisibleTodos } from '../reducers'
 
 const mapStateToProps = (state, {match}) => {
   return {
-    todos: getVisibleTodos(state.todos, match.params.filter || 'all'),
+    todos: getVisibleTodos(state, match.params.filter || 'all'),
   };
 };
-
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     onTodoClick: (id) => {
-//       dispatch(toggleTodo(id));
-//     },
-//   };
-// };
 
 const VisibleTodoList = withRouter(connect(
   mapStateToProps,
